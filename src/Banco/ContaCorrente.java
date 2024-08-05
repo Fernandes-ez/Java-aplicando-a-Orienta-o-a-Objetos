8174377f726f0ff5cb8321e28ab52fb39c7d4ef1
efernades <enzo.dantas@hotmail.com>
2024-08-05 12:35:58 -0300

    atualizações

--- src/Banco/ContaCorrente.java
+++
@@ -1,9 +0,0 @@
-package Banco;
-
-public class ContaCorrente extends ContaBancaria {
-    private double tarifa;
-    public void cobrarTarifaMensal(){
-        saldo=-tarifa;
-        System.out.println("Tarifa mensal de "+tarifa+" cobrada!");
-    }
-}
